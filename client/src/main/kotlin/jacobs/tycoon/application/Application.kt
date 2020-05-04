de54bc4a302d3f3418b69fbeea742237a277218a
Simon Jacobs <simonjacobs@btinternet.com>
2020-05-04 09:41:35 +0100

    added a cheeky to-do list

--- client/src/main/kotlin/jacobs/tycoon/application/Application.kt
+++ client/src/main/kotlin/jacobs/tycoon/application/Application.kt
@@ -7,6 +7,36 @@ import kotlinx.coroutines.launch
 import org.kodein.di.Kodein
 import org.kodein.di.erased.instance
 
+/**
+ * TODO list:
+ *
+ * * Cookie to reestablish connection
+ * * Message to ensure cookies enabled
+ * * Warning when not connected
+ * * Client attempt to reestablish connection
+ * * Add backdoor to alter game state
+ * *
+ * * GAME
+ * * Trading options
+ * * Game winner
+ * * Community/chest chance
+ * * Game phases
+ * * Dice roll
+ * * Piece moving
+ * * View rules
+ * * Mortgaging
+ * * Auctions
+ * * Go £200
+ * * Money
+ * * Triple doubles
+ * * Jail
+ * * GOOJF
+ * * Building
+ * * Selling
+ * * Rent collection
+ * * Tax
+ * *
+ */
 class Application ( kodein: Kodein ) {
 
     private val coroutineScope by kodein.instance < CoroutineScope > ()
