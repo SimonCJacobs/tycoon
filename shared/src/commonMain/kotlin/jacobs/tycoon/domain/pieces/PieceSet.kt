6f80b993bef82ca29620b42e9f1917a5e0ededc5
Simon Jacobs <simonjacobs@btinternet.com>
2020-05-07 01:23:10 +0100

    part-way through implementing the moving-the-piece phase. some nice progess on the drag and drop.

--- shared/src/commonMain/kotlin/jacobs/tycoon/domain/pieces/PieceSet.kt
+++ shared/src/commonMain/kotlin/jacobs/tycoon/domain/pieces/PieceSet.kt
@@ -1,16 +1,24 @@
 package jacobs.tycoon.domain.pieces
 
+import jacobs.tycoon.domain.board.Square
 import jacobs.tycoon.domain.players.GamePlayers
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 abstract class PieceSet {
 
     abstract val name: String
     protected abstract val pieces: List < PlayingPiece >
+    private var piecesInUse: List < PlayingPiece > = emptyList()
 
     fun count(): Int {
         return this.pieces.size
+    }
+
+    fun freezePiecesInUse( players: GamePlayers ) {
+        this.piecesInUse = players.asSortedList().map { eachPlayer -> eachPlayer.piece }
+        println( piecesInUse )
     }
 
     fun getAvailablePieces( players: GamePlayers ): List < PlayingPiece > {
@@ -18,6 +26,14 @@ abstract class PieceSet {
         return this.pieces.filter {
             false == piecesInUse.contains( it )
         }
+    }
+
+    fun getPiecesOnSquare( square: Square ): Set < PlayingPiece > {
+        return this.piecesInUse.filter { it.square == square }.toSet()
+    }
+
+    fun moveAllToSquare( square: Square ) {
+        this.piecesInUse.forEach { it.moveToSquare( square ) }
     }
 
 }
