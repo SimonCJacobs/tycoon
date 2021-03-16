7bd33081694988469fecb12fbd604cddf2c96618
Simon Jacobs <simonjacobs@btinternet.com>
2021-03-16 00:27:02 +0000

    fixing a serialization bug: allowing serialization to be done in the scope of Properties (necessary for Auctions to work) and ActionSquares (not aware this is necessary as not aware when used in serialization... but done now)

--- shared/src/commonMain/kotlin/jacobs/tycoon/domain/board/squares/SquareSerializerModule.kt
+++ shared/src/commonMain/kotlin/jacobs/tycoon/domain/board/squares/SquareSerializerModule.kt
@@ -1,23 +1,42 @@
 package jacobs.tycoon.domain.board.squares
 
+import kotlinx.serialization.modules.PolymorphicModuleBuilder
 import kotlinx.serialization.modules.polymorphic
 import kotlinx.serialization.modules.SerializersModule
 
 fun squareSerializerModule(): SerializersModule {
     return SerializersModule {
+        polymorphic( Property::class ) {
+            properties()
+        }
+        polymorphic( ActionSquare::class ) {
+            actionSquares()
+        }
         polymorphic( Square::class ) {
-            subclass( CardSquare::class, CardSquare.serializer() )
-            subclass( FreeParkingSquare::class, FreeParkingSquare.serializer() )
-            subclass( GoSquare::class, GoSquare.serializer() )
-            subclass( GoToJailSquare::class, GoToJailSquare.serializer() )
-            subclass( JailSquare::class, JailSquare.serializer() )
-            subclass( JustVisitingJailSquare::class, JustVisitingJailSquare.serializer() )
-            subclass( NullProperty::class, NullProperty.serializer() )
-            subclass( NullSquare::class, NullSquare.serializer() )
-            subclass( Station::class, Station.serializer() )
-            subclass( Street::class, Street.serializer() )
-            subclass( TaxSquare::class, TaxSquare.serializer() )
-            subclass( Utility::class, Utility.serializer() )
+            properties()
+            actionSquares()
+            squares()
         }
     }
+}
+
+private fun PolymorphicModuleBuilder < Property >.properties() {
+    subclass( NullProperty::class, NullProperty.serializer() )
+    subclass( Street::class, Street.serializer() )
+    subclass( Station::class, Station.serializer() )
+    subclass( Utility::class, Utility.serializer() )
+}
+
+private fun PolymorphicModuleBuilder < ActionSquare >.actionSquares() {
+    subclass( CardSquare::class, CardSquare.serializer() )
+    subclass( FreeParkingSquare::class, FreeParkingSquare.serializer() )
+    subclass( GoSquare::class, GoSquare.serializer() )
+    subclass( GoToJailSquare::class, GoToJailSquare.serializer() )
+    subclass( JailSquare::class, JailSquare.serializer() )
+    subclass( JustVisitingJailSquare::class, JustVisitingJailSquare.serializer() )
+    subclass( TaxSquare::class, TaxSquare.serializer() )
+}
+
+private fun PolymorphicModuleBuilder < Square >.squares() {
+    subclass( NullSquare::class, NullSquare.serializer() )
 }
